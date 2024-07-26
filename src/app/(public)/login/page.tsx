913577bebdeba3dd2df3308f8660a0a9badf639c
kalebhenrique <kalebhenriquebr@gmail.com>
2024-07-26 14:57:27 -0300

    add data table

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,15 +1,21 @@
-import { InputForm } from "@/app/_components/forms";
+import { LoginForm } from "@/app/_components/login-forms";
+import CldImage from "@/components/app/cldImage";
 
 export default function Login() {
-    return (
-        <div className="flex items-center justify-center mx-auto">
-            <div className="flex bg-gray-300 w-full h-svh">
-                teste
-            </div>
-            <div className="flex bg-white w-full h-svh items-center justify-center">
-                <InputForm/>
-            </div>
-        </div>
-    );
-  }
-  
+  return (
+    <div className="mx-auto flex items-center justify-center">
+      <div className="flex h-svh w-full items-center justify-center bg-gray-300">
+        <CldImage
+          alt={"socorro"}
+          src={"cld-sample-5"}
+          width={"500"}
+          height={"500"}
+        />
+      </div>
+      <div className="flex h-svh w-full flex-col items-center justify-center bg-white">
+        <div className="pb-20 text-2xl font-bold">Entre na sua conta</div>
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
